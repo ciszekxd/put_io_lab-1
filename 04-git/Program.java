21cfb097cc2a71afbff8fefa2ec20af4600a0fb1
KacpiCzeci <kapixxx666@wp.pl>
2020-10-29 12:12:41 +0100

    Change 1 +2

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -1,11 +1,11 @@
 //Forgotten comment
 public class MyClass {
   int x = 5;
-
-  public static void main(String[] args) {
+//Change 1
+  public static void main(String[] args) { //Change 2
     MyClass myObj = new MyClass();
     System.out.println(myObj.x);
-  }
+  }//Change 3
 }
 //Random comment
 //Change 4
